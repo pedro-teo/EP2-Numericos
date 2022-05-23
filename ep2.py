536f7b3d7a1bd9d48106ba24ef1d218a5938a4a8
pedro-teo <pedroteodoro38@gmail.com>
2022-05-23 17:24:34 -0300

    criacao do arquivo

---
+++ ep2.py
@@ -0,0 +1,7 @@
+###     MAP3121 - Calculo Numerico - Escola Politecnica                  ###
+###     Tarefa 2 - Formulas de Integracao Numerica de Gauss              ###
+
+###     Alunos deste grupo:                                              ###       
+###     Fabio Akira Yonamine   - NUSP 11805398                           ###
+###     Pedro H. Teodoro Silva - NUSP 11805314                           ###
+
